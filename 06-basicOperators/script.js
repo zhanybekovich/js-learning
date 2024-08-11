a40efa0693d9219410c81173507c7c21efd33f67
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-08-11 18:45:45 +0600

    Lesson 06-basicOperators
    Lesson 06-basicOperators

---
+++ 06-basicOperators/script.js
@@ -0,0 +1,57 @@
+/**
+ * Math operators
+ */
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+console.log(2 ** 2);
+
+/**
+ * Concatenation
+ */
+console.log("Hello " + "world");
+
+/**
+ * Assignment
+ */
+let x = 1;
+x += 1;
+x -= 1;
+x *= 1;
+x /= 1;
+x %= 1;
+x **= 1;
+
+/**
+ * Increment
+ */
+let y = 1;
+console.log(y++);
+
+let z = 1;
+console.log(++z);
+
+/**
+ * Decrement
+ */
+let a = 1;
+console.log(a--);
+
+let b = 1;
+console.log(--b);
+
+/**
+ * Comparison
+ */
+console.log(1 == 1);
+console.log(1 === 2);
+console.log(1 != 1);
+console.log(1 != 2);
+console.log(1 > 1);
+console.log(1 < 1);
+console.log(1 >= 1);
+console.log(1 <= 1);
+console.log(1 >= 2);
+console.log(1 <= 2);
